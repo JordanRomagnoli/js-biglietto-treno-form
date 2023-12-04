a712a288d56aa6ca76065a83b4e2794c8239acb5
JordanRomagnoli <jordanromagnoli1999@gmail.com>
2023-12-04 16:56:20 +0100

    - SET km-input const - SET age-input const - ADD select condition

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,30 @@
-// Test funzionamento
-console.log('Template');
+const km = document.getElementById('user-km');
+const age = document.getElementById('user-age');
+
+
+
+const submitButton = document.getElementById('submit-button');
+
+submitButton.addEventListener('click', function(){
+    const kmInput = parseInt(km.value);
+    const ageValue = parseInt(age.value);
+
+
+    let prize = parseFloat((km.value * 0.21));
+    
+    
+
+    if (ageValue < 18){
+        console.log (prize * 0.8);
+    }
+    
+    else if(ageValue > 65){
+        console.log (prize * 0.6);
+
+    }else{
+        console.log (prize);
+    }
+
+
+}
+);
