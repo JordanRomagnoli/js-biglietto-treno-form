b1adc8b8d4ce32045dc2c5e2e935b132799c0f30
JordanRomagnoli <jordanromagnoli1999@gmail.com>
2023-12-04 18:50:45 +0100

    - STAMP values inside document - ADD style

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,4 @@
+const entireName = document.getElementById('user-name');
 const km = document.getElementById('user-km');
 const age = document.getElementById('user-age');
 
@@ -6,23 +7,39 @@ const age = document.getElementById('user-age');
 const submitButton = document.getElementById('submit-button');
 
 submitButton.addEventListener('click', function(){
+    const nameInput = (entireName.value);
     const kmInput = parseInt(km.value);
     const ageValue = parseInt(age.value);
+    const wagon = Math.floor(Math.random()*10)+1; 
+    const code = Math.floor(Math.random()*10000)+1;
+    
 
 
-    let prize = parseFloat((km.value * 0.21));
+    let prize = parseFloat((km.value * 0.21)).toFixed(2);
+    let discount20 = (prize * 0.8).toFixed(2);
+    let discount40 = (prize * 0.6).toFixed(2);
     
-    
+    document.getElementById('name-container').innerHTML = `${nameInput}`;
+    document.getElementById('wagon-container').innerHTML = `${wagon}`;
+    document.getElementById('code-container').innerHTML = `${code}`;
+
 
     if (ageValue < 18){
-        console.log (prize * 0.8);
+        document.getElementById('price-container').innerHTML = `${discount20}€`;
+        document.getElementById('discount-container').innerHTML = `Biglietto Under 18`;
+
     }
     
     else if(ageValue > 65){
-        console.log (prize * 0.6);
+        document.getElementById('price-container').innerHTML = `${discount40}€`;
+        document.getElementById('discount-container').innerHTML = `Biglietto Over 65`;
+
 
     }else{
-        console.log (prize);
+        document.getElementById('price-container').innerHTML = `${prize}€`;
+        document.getElementById('discount-container').innerHTML = `Biglietto Standard`;
+
+
     }
 
 
